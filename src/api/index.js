87af0a1a4db21c1be521a387574d4fd1249a0250
adev07 <adityaanand1245@gmail.com>
2024-04-07 20:27:31 +0530

    changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'http://localhost:5000/posts';
+const url = 'https://memories-app-backend-9cd1.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
